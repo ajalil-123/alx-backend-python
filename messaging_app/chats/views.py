1e92060feb7847ecf3fba7a6e0cf4bad69c39884
Jalil <abduljalilzakaria1@gmail.com>
2025-07-26 22:14:19 +0200

    create api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework import viewsets, status
+# from rest_framework.response import Response
+# from rest_framework.decorators import action
+# from .models import Conversation, Message, CustomUser
+# from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+
+# chats/views.py
+
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
