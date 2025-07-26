31d93b1e6b8c95b2fa3433b6680ebd287737d819
Jalil <abduljalilzakaria1@gmail.com>
2025-07-26 22:23:18 +0200

    create api endpoints with views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,21 +1,16 @@
-# from django.shortcuts import render
-# from rest_framework import viewsets, status
-# from rest_framework.response import Response
-# from rest_framework.decorators import action
-# from .models import Conversation, Message, CustomUser
-# from .serializers import ConversationSerializer, MessageSerializer
-
-
-# chats/views.py
-
 from rest_framework import viewsets
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['participants']
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['conversation', 'sender']
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -126,3 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'chats.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
