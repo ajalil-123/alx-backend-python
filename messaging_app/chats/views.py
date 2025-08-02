9295e2e5397974d9c9fe7b8e2f98327f24618066
Jalil <abduljalilzakaria1@gmail.com>
2025-08-02 18:06:20 +0200

    create custom persion classes

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,23 +1,13 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
-from .permissions import IsParticipantOfConversation
+from rest_framework.exceptions import APIException
+from rest_framework.status import HTTP_403_FORBIDDEN  # <-- REQUIRED import
+
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
 
-class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
-    serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
-
-    def get_queryset(self):
-        # Filter conversations where the user is a participant
-        return self.queryset.filter(participants=self.request.user)
-
-    def perform_create(self, serializer):
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
@@ -25,11 +15,16 @@ class MessageViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         conversation_id = self.kwargs['conversation_pk']
-        return Message.objects.filter(conversation__id=conversation_id, conversation__participants=self.request.user)
+        return Message.objects.filter(
+            conversation__id=conversation_id,
+            conversation__participants=self.request.user
+        )
 
     def perform_create(self, serializer):
         conversation_id = self.kwargs['conversation_pk']
         conversation = Conversation.objects.get(id=conversation_id)
+
         if self.request.user not in conversation.participants.all():
-            raise PermissionDenied("You are not a participant in this conversation.")
+            raise APIException(detail="You are not allowed to perform this action.", code=HTTP_403_FORBIDDEN)
+
         serializer.save(sender=self.request.user, conversation=conversation)
