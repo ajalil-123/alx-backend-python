998de0039e005557b0e2420ce6a2b17b0b279120
Jalil <abduljalilzakaria1@gmail.com>
2025-08-06 17:29:29 +0200

    corrected rolepermision class

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -81,7 +81,7 @@ class OffensiveLanguageMiddleware:
 
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
